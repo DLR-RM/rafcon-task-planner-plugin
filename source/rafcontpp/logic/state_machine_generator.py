a13c007b8595a5659dcb26517dd2f1e182df7b05
Christoph Sürig <christoph.suerig@dlr.de>
2019-08-21 17:57:11 +0200

    doc(state_machine_generator): formatted sm_generator

--- source/rafcontpp/logic/state_machine_generator.py
+++ source/rafcontpp/logic/state_machine_generator.py
@@ -10,46 +10,43 @@
 # Version 21.08.1019
 
 
-
 import copy
+import json
 import os
 import time
-import json
-from rafcontpp.logic.state_machine_layouter import StateMachineLayouter
-from rafcontpp.model.datastore import ALLOW_OVERRIDE_NAME, SEMANTIC_DATA_DICT_NAME
-from rafcontpp.model import interruptable_thread
-from rafcon.gui.models.signals import ActionSignalMsg
-from rafcon.gui.singleton import state_machine_manager_model
-from rafcon.gui.utils import wait_for_gui
-from rafcon.gui.config import global_gui_config
-from rafcon.gui.helpers.state import substitute_state, substitute_state_as
-from rafcon.core.storage import storage
+
 from rafcon.core.singleton import library_manager
 from rafcon.core.singleton import state_machine_manager
 from rafcon.core.state_machine import StateMachine
-from rafcon.core.states.hierarchy_state import HierarchyState
 from rafcon.core.states.execution_state import ExecutionState
-from rafcon.utils.gui_functions import call_gui_callback
+from rafcon.core.states.hierarchy_state import HierarchyState
+from rafcon.core.storage import storage
+from rafcon.gui.helpers.state import substitute_state_as
+from rafcon.gui.models.signals import ActionSignalMsg
+from rafcon.gui.singleton import state_machine_manager_model
+from rafcon.gui.utils import wait_for_gui
 from rafcon.utils import log
-from rafcon.gui.models.state_machine import StateMachineModel
+from rafcon.utils.gui_functions import call_gui_callback
+from rafcontpp.logic.state_machine_layouter import StateMachineLayouter
+from rafcontpp.model import interruptable_thread
+from rafcontpp.model.datastore import ALLOW_OVERRIDE_NAME, SEMANTIC_DATA_DICT_NAME
+
 logger = log.get_logger(__name__)
 
 
 class StateMachineGenerator:
-    '''StateMachineGenerator
+    """StateMachineGenerator
     The StateMachineGenerator takes the plan from the datastore and molds a state machine out of it.
-
-    '''
+    """
 
     def __init__(self, datastore):
-            self.__datastore = datastore
+        self.__datastore = datastore
 
     def generate_state_machine(self):
-        ''' generate_state_machine
+        """ generate_state_machine
         generate_state_machine generates a state machine, fills the data ports and opens the state machine in rafcon,
         if it's a new one.
-
-        '''
+        """
         target_state = self.__datastore.get_target_state()
         generate_independent = target_state == None
         if target_state:
@@ -83,12 +80,12 @@ class StateMachineGenerator:
             target_state = HierarchyState(name=sm_name, state_id=old_sm_rs_id)
         target_state, state_order_list = self.__generate_core_machine(target_state)
         state_machine = StateMachine(root_state=target_state)
-        storage.save_state_machine_to_path(state_machine,sm_path)
+        storage.save_state_machine_to_path(state_machine, sm_path)
         logger.info('State machine "{}" created.'.format(state_machine.root_state.name))
         logger.info(sm_name + " contains " + str(len(target_state.states)) + " states.")
         logger.info("State machine generation took {0:.4f} seconds.".format(time.time() - start_time))
         sm_layouter = StateMachineLayouter()
-        sm_layouter.layout_state_machine(state_machine,target_state,False,state_order_list)
+        sm_layouter.layout_state_machine(state_machine, target_state, False, state_order_list)
         return state_machine
 
     def __generate_state_machine_into_state(self, target_state):
@@ -104,28 +101,29 @@ class StateMachineGenerator:
         valid, error = self.__is_valid_target_state(target_state)
         state_to_generate_into = None
         successful = False
-        if  self.__is_valid_target_state(target_state):
+        if self.__is_valid_target_state(target_state):
             state_machine = target_state.get_state_machine()
             state_machine_m = state_machine_manager_model.state_machines[state_machine.state_machine_id]
             target_state_m = state_machine_m.get_state_model_by_path(target_state.get_path())
             target_state_copy = copy.deepcopy(target_state)
             if self.__clear_state(target_state_copy):
                 if not target_state.is_root_state:
-                    target_state_copy, state_order_list =  self.__generate_core_machine(target_state_copy)
+                    target_state_copy, state_order_list = self.__generate_core_machine(target_state_copy)
                     call_gui_callback(substitute_state_as, target_state_m, target_state_copy, False)
                     wait_for_gui()
                     if state_machine.file_system_path:
                         storage.save_state_machine_to_path(state_machine, state_machine.file_system_path)
                     logger.info('State machine generated into state "{}" of state machine "{}".'
                                 .format(target_state_copy.name, state_machine.root_state.name))
-                    logger.info('"{}" contains {} states.'.format(target_state_copy.name, len(target_state_copy.states)))
+                    logger.info(
+                        '"{}" contains {} states.'.format(target_state_copy.name, len(target_state_copy.states)))
                     logger.info("State machine generation took {0:.4f} seconds.".format(time.time() - start_time))
                     sm_layouter = StateMachineLayouter()
                     target_state_m_copy = state_machine_m.get_state_model_by_path(target_state_copy.get_path())
                     target_state_m_copy.action_signal.emit(ActionSignalMsg(action='substitute_state', origin='model',
-                                                                        action_parent_m=target_state_m_copy,
-                                                                        affected_models=[target_state_m_copy],
-                                                                        after=False))
+                                                                           action_parent_m=target_state_m_copy,
+                                                                           affected_models=[target_state_m_copy],
+                                                                           after=False))
                     call_gui_callback(sm_layouter.layout_state_machine, state_machine,
                                       target_state_copy, True, state_order_list)
                     wait_for_gui()
@@ -143,21 +141,18 @@ class StateMachineGenerator:
                 logger.error('Could\'nt clear target state.')
         else:
             logger.error(error)
-
         return successful, state_to_generate_into
 
-    def __clear_state(self,state):
+    def __clear_state(self, state):
         """
         clear_state receives a hierarchy state, and removes all child states.
         :param state: a hierarchy state
         :return: true if clearing was successful, false otherwhise.
         """
         for child_state in state.states.values():
-           call_gui_callback(state.remove_state, child_state.state_id, True, True, True)
+            call_gui_callback(state.remove_state, child_state.state_id, True, True, True)
         wait_for_gui()
         return len(state.states) == 0
-
-
 
     def __is_valid_target_state(self, target_state):
         """
@@ -171,26 +166,23 @@ class StateMachineGenerator:
         if target_state is None:
             is_valid = False
             error_message = 'Can\'t plan into None.'
-        elif not isinstance(target_state,HierarchyState):
-             is_valid = False
-             error_message = 'Can\'t Plan into State {}, can only plan into HierarchyStates!'.format(target_state)
+        elif not isinstance(target_state, HierarchyState):
+            is_valid = False
+            error_message = 'Can\'t Plan into State {}, can only plan into HierarchyStates!'.format(target_state)
         elif len(target_state.states) > 0:
             permission = target_state.semantic_data[SEMANTIC_DATA_DICT_NAME][ALLOW_OVERRIDE_NAME]
             if not permission or permission.lower() == 'true':
                 is_valid = False
                 error_message = "Can't plan into None empty HierarchyState without permission!"
-        return  is_valid, error_message
-
-
-
+        return is_valid, error_message
 
     def __generate_core_machine(self, target_state):
-        '''
+        """
         takes a root state, and generates the state machine into it.
         :param target_state: the root state
         :return:(root_state, state_order list) the root state containing the state machine, and the state order list is a list
         of all states in the sm in right order. Can return (None,[]) if process was interrupted.
-        '''
+        """
         a_s_map = self.__datastore.get_action_state_map()
         pddl_action_dict = self.__datastore.get_pddl_action_map()
         facts = self.__datastore.get_pddl_facts_representation()
@@ -205,7 +197,6 @@ class StateMachineGenerator:
             target_state.add_state(last_state)
             target_state.set_start_state(last_state.state_id)
             state_order_list.append(last_state.state_id)
-
         for plan_step in self.__datastore.get_plan():
             # the name of a plan step is an action name.
             if current_thread and current_thread.is_interrupted():
@@ -231,7 +222,7 @@ class StateMachineGenerator:
                 target_state.add_state(current_state)
                 # add the state to the order list (for later formatting)
                 state_order_list.append(current_state.state_id)
-                #add transitions.
+                # add transitions.
                 if last_state is None:
                     target_state.set_start_state(current_state.state_id)
                 else:
@@ -241,24 +232,21 @@ class StateMachineGenerator:
             else:
                 logger.error("No State found for action: \"" + plan_step.name + "\"")
                 raise LookupError("No State found for action: \"" + plan_step.name + "\"")
-
         # at the end add transition from last state to outcome of root state.
         target_state.add_transition(last_state.state_id, 0, target_state.state_id, 0)
         library_manager.refresh_libraries()
-
         if not current_thread or current_thread.is_interrupted():
             target_state = None
             state_order_list = []
-
         return target_state, state_order_list
 
     def __load_state(self, wanted_state):
-        '''load_state
+        """load_state
         load_state gets a state and loads it from the libraries.
         :param self:
         :param wanted_state: a state that should be loaded
         :return: the loaded state
-        '''
+        """
         state_libs = self.__datastore.get_state_pools()
         libraries = {}
         lib_names = []
@@ -267,22 +255,20 @@ class StateMachineGenerator:
             lib_name = os.path.basename(os.path.abspath(pool))
             lib_names.append(lib_name)
             libraries[lib_name] = os.path.abspath(pool)
-
         for lib_name in lib_names:
             state_pool = library_manager.libraries[lib_name]
             if wanted_state in state_pool:
                 return_state = library_manager.get_library_instance(lib_name, wanted_state)
                 break
-
         return return_state
 
     def __open_state_machine(self, state_machine, state_machine_path):
-        '''
+        """
         gets a state machine and opens it in rafcon. If an old version is still open, it closes it first.
         :param state_machine: the name of the state machine
         :param state_machine_path: the path of the state machine.
         :return:
-        '''
+        """
         logger.info('Opening state machine...')
         if state_machine_manager.is_state_machine_open(state_machine.file_system_path):
             old_sm = state_machine_manager.get_open_state_machine_of_file_system_path(state_machine.file_system_path)
@@ -291,14 +277,13 @@ class StateMachineGenerator:
         call_gui_callback(state_machine_manager.add_state_machine, new_state_machine)
 
     def __get_runtime_data_init_state(self, data_init_file_path, use_as_ref):
-        '''
+        """
         creates an execution state, containing the, or a reference to the runtime data, as well as the code needed
         to write it into a dictionary in the global variables.
         :param data_init_file_path: The path of a file containing a json dict
         :param use_as_ref: True if the path should be included as reference, False if the dictionary itself should be included.
         :return: an Execution state, that will update the rtpp_data dict in the global variables.
-        '''
-
+        """
         data_init_state = ExecutionState(name='Runtime Data Initialization (rtpp_data)')
         data_to_load = None
         if use_as_ref:
@@ -309,18 +294,15 @@ class StateMachineGenerator:
         execute_str = "def execute(self, inputs, outputs, gvm):\r\n"
         execute_str = "{}    self.logger.info('Updating rtpp_data.')\r\n".format(execute_str)
         execute_str = "{}    rtpp_data = gvm.get_variable('rtpp_data')\r\n".format(execute_str)
-        execute_str = "{}    rtpp_data = rtpp_data if rtpp_data else {}\r\n".format(execute_str,{})
+        execute_str = "{}    rtpp_data = rtpp_data if rtpp_data else {}\r\n".format(execute_str, {})
         execute_str = "{}    rtpp_data.update({})\r\n".format(execute_str, data_to_load)
-        execute_str = '{}    gvm.set_variable(\'{}\',{})\r\n'.format(execute_str,'rtpp_data',' rtpp_data')
+        execute_str = '{}    gvm.set_variable(\'{}\',{})\r\n'.format(execute_str, 'rtpp_data', ' rtpp_data')
         execute_str = "{}    return \"success\"".format(execute_str)
         data_init_state.script_text = execute_str
         return data_init_state
-
-
 
     def __get_actual_sm_name(self):
         sm_name = self.__datastore.get_sm_name()
         sm_name = self.__datastore.get_pddl_facts_representation().problem_name + '_state_machine' if len(
             sm_name) == 0 else sm_name
         return sm_name
-
