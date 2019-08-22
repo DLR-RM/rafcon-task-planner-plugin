997991bb9ac7cab1e01b0ca424aaef023ee7f9df
Christoph Sürig <christoph.suerig@dlr.de>
2019-08-22 14:16:18 +0200

    doc(turtle_sim): changed from library to hierarchy state
    changed the core machine from dedicated state machine
    to hierarchy state in usual machine.

--- examples/turtle_sim_example/src/turtle_sim_core_machine/turtle_sim_core_machine_ZXVECE/dummy_Core_LZYVPA/script.py
+++
@@ -1,4 +0,0 @@
-
-def execute(self, inputs, outputs, gvm):
-    self.logger.info("Hello, this is the dummy core machine. If you see this, you did not plan yet, or didn't refreh the the imported library core machine.")
-    return 0
